10fead9dcd776de2ebd012a2af8c7e0220996c57
salino3 <programacionflavio@gmail.com>
2023-11-04 09:30:44 +0100

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("My awesome books portal");
+});
+
+app.listen(3000, () => {
+  console.log("Server ready at port 3000");
+});
